c24b5fd3f3c23ff74a115d68d8ffdabe9451e808
Narasimha <murthyqa.n@gmail.com>
2025-09-27 11:17:53 +0530

    new file

--- NewVariables.sh
+++ NewVariables.sh
@@ -4,5 +4,5 @@ echo "currenct directory is : $PWD"
 echo "who is running the script : $USER"
 echo "Home directory of user is : $HOME"
 echo "PID of user is : $$"
-Sleep 50 &
+Sleep 50
 echo "PID of last background process is : $!"
