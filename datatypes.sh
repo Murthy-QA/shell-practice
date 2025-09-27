43bc041501a9c951644996a619bf1a310731ce5e
Narasimha <murthyqa.n@gmail.com>
2025-09-27 11:27:11 +0530

    new file

--- datatypes.sh
+++ datatypes.sh
@@ -7,8 +7,8 @@ SUM=$(($NUM1+$NUM2))
 
 echo "Sum is : $SUM"
 
-Flowers={"Rose""Jasmine""Sunflower""RedLove"}
-echo "Flowers are : $Flowers[0]"
+Flowers={"Rose" "Jasmine" "Sunflower" "RedLove"}
+echo "Flowers are : ${Flowers[0]}"
 
 
 
