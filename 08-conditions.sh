09dc36adb7d35c915592c5219c2233221cd95890
Narasimha <murthyqa.n@gmail.com>
2025-10-06 23:09:26 +0530

    commit msg

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,7 +2,7 @@
 
 echo "Enter a Number"
 read NUMBER
-if [ $(($NUMBER)) -eq 10 ]; then
+if [$(($NUMBER)) -eq 10 ]; then
     echo "Given number $NUMBER is equal to 10"
 else
     echo "Given number $NUMBER is not equal to 10"
