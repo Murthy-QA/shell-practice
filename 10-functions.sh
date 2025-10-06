50cfe76f9e6cf61cbfc2060d0ee216dea05e59bf
Narasimha <murthyqa.n@gmail.com>
2025-10-06 23:30:33 +0530

    commit msg

---
+++ 10-functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: Please run the script with root"
+    exit 1
+fi
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]; then
+        echo "INSTALLING: $2 is failure"
+        exit 1
+    else
+    echo "$2 installed successfully"
+    fi
+}
+
+dnf install python3 -y
+VALIDATE $? "PYTHON"
+
+dnf install zip -y
+VALIDATE $? "ZIP"
