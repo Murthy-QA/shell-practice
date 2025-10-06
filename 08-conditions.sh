c147f2ea8fbec8563d471134a572251a3396bb0d
Narasimha <murthyqa.n@gmail.com>
2025-10-06 22:57:44 +0530

    commit msg

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 read NUMBER
-if [$(($NUMBER -eq 100)]; then
+if [$(($NUMBER -eq 100))]; then
     echo "Number is equal to 100 : $NUMBER"
 else
     echo "Number is not equal to 100 : $NUMBER"
 fi
-
