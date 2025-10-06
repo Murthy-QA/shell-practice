25c3d8348e21165341bf8f89a9c3287fe2eb08d9
Narasimha <murthyqa.n@gmail.com>
2025-10-06 22:31:18 +0530

    commit msg

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 NAME="NARASIMHA"
 SURNAME="CH"
+PERSON1="RAMESH"
+PERSON2="SURESH"
 
 echo "my name is : $NAME"
 echo "my sur name is : $SURNAME"
+echo "this person1 is $PERSON1"
+echo "This person2 is $PERSON2"
