238469a8c32e8d91ae5797d4cb275e25aa4a9950
Narasimha <murthyqa.n@gmail.com>
2025-10-06 23:04:18 +0530

    commit msg

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,7 +2,7 @@
 
 echo "Enter a Number"
 read NUMBER
-if [($NUMBER) -le 10]; then
+if [$(($NUMBER)) -le 10]; then
     echo "Number is less than 10"
 else
     echo "Number is greater than or equal to 10"
