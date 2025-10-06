3ae1d4681033f432b29e8ee8de4f424d0e9f73ac
Narasimha <murthyqa.n@gmail.com>
2025-10-06 22:50:45 +0530

    commit msg

---
+++ 07-datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+FRUITS_NAMES=("APPLE" "GUAVA" "PAPAYA" "POMEGRANTE")
+NUMBER3=$(($NUMBER1+$NUMBER2))
+echo "Adding of two numbers is $NUMBER3"
+
+echo "First Fruit name : $FRUITS_NAME[$0]"
