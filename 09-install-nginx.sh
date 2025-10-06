ef0ef3a90554c74268f9c1e0c4d7e60648232073
Narasimha <murthyqa.n@gmail.com>
2025-10-06 23:19:43 +0530

    commit msg

--- 09-install-nginx.sh
+++ 09-install-nginx.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0]; then
     echo "ERROR: Please run the script with root"
     exit 1
+fi
 
 dnf install nginx -y
 
@@ -13,3 +14,4 @@ if [$? -ne 0]; then
     exit 1
 else
     echo "MYSQL installed successfully"
+fi
