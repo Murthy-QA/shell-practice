1fb241f818e2baf248052a1f7ddcd24d59be91d3
Narasimha <murthyqa.n@gmail.com>
2025-10-06 23:12:44 +0530

    commit msg

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,7 +4,7 @@ echo "Enter a Number"
 read NUMBER
 if [ $(($NUMBER)) -le 100 ]; then
     echo "Given number $NUMBER is less than 100"
-elif[ $(($NUMBER)) -eq 100 ]; then
+elif [ $(($NUMBER)) -eq 100 ]; then
     echo "Given number $NUMBER is  equal to 100"
 else
     echo "Given number $NUMBER is  greater than 100"
