31c4f60d217eeb0d2e5b44ef330216a8cb1ee3c3
Narasimha <murthyqa.n@gmail.com>
2025-10-06 22:36:22 +0530

    commit msg

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,3 +3,8 @@
 echo " what is my name"
 read NAME
 echo "my name is : $NAME"
+
+echo "Please enter your PIN number"
+read -s PIN_NUMBER # here PIN_NUMBER is variable
+
+echo "PIN entered is: $PIN_NUMBER"
