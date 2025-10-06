af96618b00fcd5e72af7cf9044bedec42269c017
Narasimha <murthyqa.n@gmail.com>
2025-10-06 23:00:14 +0530

    commit msg

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,7 +2,7 @@
 
 echo " Enter a Number"
 read NUMBER
-if [($NUMBER -le 100)]; then
+if [$NUMBER -le 100]; then
     echo "Number is less than 100"
 else
     echo "Number is greater than or equal to 100"
