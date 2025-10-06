7840f17131ccc4d855920cec02c44d0d760b5fd1
Narasimha <murthyqa.n@gmail.com>
2025-10-06 23:26:24 +0530

    commit msg

--- 09-install-nginx.sh
+++ 09-install-nginx.sh
@@ -15,3 +15,12 @@ if [ $? -ne 0 ]; then
 else
     echo "NGINX installed successfully"
 fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MySQL is failure"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
