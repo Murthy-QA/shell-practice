4a40502292c0e2bea735d1d0fea4213249e743ec
Narasimha <murthyqa.n@gmail.com>
2025-10-07 16:54:20 +0530

    new script added

--- 17-Count.sh
+++ 17-Count.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-x="NARASIMHAMURTHY"
-grep -o "A" <<<"$x" | wc -l
+# x="NARASIMHAMURTHY"
+# grep -o "A" <<<"$x" | wc -l
 
-echo "Enter a Number"
-read NUMBER
-if [ $NUMBER -gt 0 ]; then
-    echo "It is a postive number"
-elif [ $NUMBER -lt 0 ]; then
-    echo "It is a negative number"
-else
-    echo "It is number with 0"
-fi
+# echo "Enter a Number"
+# read NUMBER
+# if [ $NUMBER -gt 0 ]; then
+#     echo "It is a postive number"
+# elif [ $NUMBER -lt 0 ]; then
+#     echo "It is a negative number"
+# else
+#     echo "It is number with 0"
+# fi
+
+FRUITS_NAMES=("APPLE" "BANANA" "GUAVA" "PINEAPPLE" "ORANGE")
+for fruit in $FRUITS_NAMES[@]
+do
+    echo "Fruit name is : $FRUITS_NAMES[fruit]
+done
