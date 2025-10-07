e009fc25db12a06b65f7562dbad96e3abf57ff3f
Narasimha <murthyqa.n@gmail.com>
2025-10-07 16:50:47 +0530

    new script added

--- 17-Count.sh
+++ 17-Count.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 x="NARASIMHAMURTHY"
 grep -o "A" <<<"$x" | wc -l
+
+echo "Enter a Number"
+read NUMBER
+if [ $NUMBER -gt 0 ]; then
+    echo "It is a postive number"
+elif [ $NUMBER -lt 0 ]; then
+    echo "It is a negative number"
+else
+    echo "It is number with 0"
+fi
