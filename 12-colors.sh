434fb7a03ea7b1343dd427200bbb0a6cc3fa13f3
Narasimha <murthyqa.n@gmail.com>
2025-10-07 09:30:43 +0530

    new script added

---
+++ 12-colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "$G This is sample colored line $N"
+echo "$R This is sample Red Line $N"
+echo "$Y This is sample Yellow Line $N"
+
